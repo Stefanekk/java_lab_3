2feba781afeef3ab8e66098969d4255baaf9d935
Bartek Stefański <92265595+Stefanekk@users.noreply.github.com>
2022-01-22 11:05:47 +0100

    cw_2

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,26 @@ public class Main {
             c) wyświetlić dane z tablicy, użyć pętli for lub forEach
             d) dane powinny być estetycznie przedstawione
          */
+        Student[] obj = new Student[3];
+
+        Student student1 = new Student("Student1", "Student1", 1, true);
+
+        Student student2 = new Student("Student2", "Student2", 2, false);
+
+        Student student3 = new Student("Student3", "Student3", 3, true);
+
+
+        obj[0] = student1;
+        obj[1] = student2;
+        obj[2] = student3;
+
+        for (Student object : obj){
+            System.out.println("Pole1: " + object.pole1);
+            System.out.println("Pole2: " + object.pole2);
+            System.out.println("Pole int: " + object.poleInt);
+            System.out.println("Pole Boolean: " + object.poleBol);
+            System.out.println("\n");
+        }
+
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    public String pole1;
+    public String pole2;
+    public int poleInt;
+    public boolean poleBol;
+
+    public Student(String pole1, String pole2, int poleInt, boolean poleBol){
+        this.pole1 = pole1;
+        this.pole2 = pole2;
+        this.poleInt = poleInt;
+        this.poleBol = poleBol;
+
+    }
+
+
+
+}
