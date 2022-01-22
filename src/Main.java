72bcfe40f5824566f183bc49ecd3c1e33d0b539f
Bartek Stefański <92265595+Stefanekk@users.noreply.github.com>
2022-01-22 10:32:47 +0100

    cw_1

--- src/Main.java
+++ src/Main.java
@@ -16,9 +16,47 @@ public class Main {
         do podanych cyfr przez użytkownika,
         g) gdy użytkownik wciśnie 0 ma wyświetlić się status KONIEC w innym wypadku KONTYNUUJEMY :),
         */
+        Scanner scan = new Scanner(System.in);
+
+        while (true){
+           int temp = scan.nextInt();
+           if (temp == 0){
+               System.out.println(StatusEnum.KONIEC);
+               break;
+           }
+           switch (temp){
+               case 1:
+                   System.out.println(LiczbyEnum.JEDEN);
+                   break;
+               case 2:
+                   System.out.println(LiczbyEnum.DWA);
+                   break;
+               case 3:
+                   System.out.println(LiczbyEnum.TRZY);
+                   break;
+               case 4:
+                   System.out.println(LiczbyEnum.CZTERY);
+                   break;
+               case 5:
+                   System.out.println(LiczbyEnum.PIEC);
+                   break;
+               case 6:
+                   System.out.println(LiczbyEnum.SZESC);
+                   break;
+               default:
+                   System.out.println(StatusEnum.KONTYNUUJEMY);
+           }
+        }
+
 
         /* statusy i wybór ze switch case to najczęstsze użycia enumów, enumy są często używane
         * w różnego rodzaju listach, selectach jako krótkie opcje wyboru np. jako status A, X (Aktualny, Usunięty) */
 
+    }
+    enum LiczbyEnum{
+        JEDEN, DWA, TRZY, CZTERY, PIEC, SZESC
+    }
+    enum StatusEnum{
+        KONTYNUUJEMY, KONIEC
     }
 }
